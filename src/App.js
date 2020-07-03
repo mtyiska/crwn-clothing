fbdd9c1acb57b439dda7d3186807d2dd730f0f5b
michaeltyiska <mtyiska@gmail.com>
2020-07-03 16:50:45 -0700

    implemented store authenticated users in firestore db

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from "./components/header/header";
 import "./App.css";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends Component {
   constructor() {
@@ -19,8 +19,23 @@ class App extends Component {
 
   unsubscribeFromAuth = null;
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState(
+          { currentUser: userAuth },
+          console.log(this.state.currentUser)
+        );
+      }
     });
   }
 
