3b0ef3e799b6a4382fdbab6542b6d1a5d7d94766
michaeltyiska <mtyiska@gmail.com>
2020-07-03 09:22:43 -0700

    update app with shop page, collection preview component, collection item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ShopPage from "./pages/shop/Shop";
 
 import "./App.css";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,7 @@ import "./homepage.styles.scss";
 
 import Directory from "../components/directory-menu/directory";
 
-export default function HomePage() {
+export default function HomePage(props) {
   return (
     <div className="homepage">
       <Directory />
